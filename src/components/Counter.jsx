b62e36f1b63a792071fa9305b5be3a663e515eb7
Brad Henderson <elliotbradly@gmail.com>
2025-09-01 12:50:10 -0400

    update

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,89 +1,15 @@
-import React, { useEffect, useState } from 'react';
 
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  const loadShade = () => {
-
-    function loadScript(src) {
-      return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.onload = () => resolve();
-        script.onerror = () => reject(new Error(`Failed to load script: ${src}`));
-
-        document.head.appendChild(script);
-      });
-    }
-
-    // Usage:
-    loadScript('./jsx/110.shade.js')
-      .then(async () => {
-        console.log('Script loaded successfully!');
-        console.log("REALLY " + SHADE)
-
-        var bit
-
-        bit = await SHADE.hunt("[Write action] Write Surface", { idx: "surface00" })
-        bit = await SHADE.hunt("[Read action] Read Container", { idx: "can-00" })
-
-        var container = bit.canBit.dat.bit;
-
-        bit = await SHADE.hunt("[Write action] Write Text", { idx: "txt00", dat: { txt: 'hi', can: 'can-00' } })
-        var text = bit.txtBit.dat.bit
-        container.addChild(text)
-
-        bit = await SHADE.hunt("[Write action] Write Text", { idx: "txt01", dat: { y: 10, txt: 'hi', can: 'can-00' } })
-        var text = bit.txtBit.dat.bit
-        container.addChild(text)
-
-        bit = await SHADE.hunt("[Write action] Write Text", { idx: "txt02", dat: { y: 30, txt: 'hi', can: 'can-00' } })
-        var text = bit.txtBit.dat.bit
-        container.addChild(text)
-
-        // Now you can safely use functions/variables from the script
-      })
-      .catch(error => {
-        console.error('Error loading script:', error);
-      });
-
-  }
-
-  //useEffect(() => {
-   // loadShade()
-  //}, []);
-
-
-  const increment = () => {
-
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-
-
-
-
+const Product = () => {
+  
 
   return (
-    <div>
-
-
-
-
-      <h2>Counter</h2>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={loadShade}>Load Shade</button>
-      <div id='surface00' />
-
-
-    </div>
+    <ul className="grid grid-cols-1 gap-4 py-2 list-disc list-inside md:grid-cols-3">
+    
+    REACT LIVE HERE
+    
+    </ul>
   );
-}
+};
 
-export default Counter;
+
+export default Product;
