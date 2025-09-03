ba44a50d5c167ebe529ce03922eed4a8a89512fc
Brad Henderson <elliotbradly@gmail.com>
2025-09-03 18:27:34 -0400

    update

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -56,21 +56,13 @@ const Product = () => {
 
   return (
 
-    <div id='surface00'>
+   
 
       <ul className="grid grid-cols-1 gap-4 py-2 list-disc list-inside md:grid-cols-3">
 
-        REACT LIVE HERE
-
+<div id='surface00'></div>
       </ul>
 
-      <button onClick={open}>BUTTOn </button>
-
-
-
-
-
-    </div>
   );
 };
 
